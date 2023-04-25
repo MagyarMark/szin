06f686e8740140370ce3487695e78b14bb4617d6
MagyarMark <magyar.mark.jozsef@diak.szbi-pg.hu>
2023-04-25 20:30:42 +0200

    main

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-színek = ["piros","zöld","kék","fekete"]
+import random
+
+színek = ["piros","zöld","kék","fekete","fehér", "sárga", "narancssárga", "rózsaszín", "lila"]
+random_színek = random.sample(színek, 4)
 
 print(f"\nŰdv a Színözön játékban!")
 print(f"A játék lényege, hogy van 4db szín amit ki kell találnod!")
+print(f"A játékban található színek: piros, zöld, kék, fekete, fehér, sárga, narancssárga, rózsaszín és lila.")
 print(f"\nHa eltalálsz egy színt akkor a játék ki írja azt, hogy: A(z) xy színt Megtaláltad, Gratulálok. ")
 print(f"Ha pedig olyan színt írsz bele ami nincs benne akkor a ki írja azt, hogy: ílyen szín nincs benne/Bíztos színt írtál be?")
 print(f"És ha mindegyik szín megvan akkor ki írja azt, hogy: Nincs több szín mindet meg találtad!")
 print(f" ")
 
-while len(színek) > 0:
+while len(random_színek) > 0:
   színekk = input("Adj meg egy színt (vagy 'q' kilépésez):")
 
   if színekk == 'q':
     break
 
-  if színekk in színek:
-    színek.remove(színekk)
+  if színekk in random_színek:
+    random_színek.remove(színekk)
     print("A(z)",színekk,"színt Megtaláltad, Gratulálok")
   else:
     print("ílyen szín nincs benne/Bíztos színt írtál be?")
 
-if len(színek) == 0:
+if len(random_színek) == 0:
   print("Nincs több szín mindet meg találtad!")
